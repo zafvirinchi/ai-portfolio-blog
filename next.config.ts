2df850e1727494b7098f8f968633d7f8600fb012
Zafrul Islam <zafrul.techstack75@gmail.com>
2026-05-23 18:09:50 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
